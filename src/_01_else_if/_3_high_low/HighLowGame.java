bba60fd8a10313a2b42d4cec5de87d7eb92627f9
root <root@de4f3e3666b9>
2021-01-09 20:56:56 +0000

    Level 0 module 3

--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -3,29 +3,40 @@ package _01_else_if._3_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100);
 		
 		// 2. Print out the random variable above
-		
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+		for(int i = 0; i < 10; i++) {
 			
+			// 1. Ask the user for a guess using a pop-up window, and save their response 
+			String guessString = JOptionPane.showInputDialog("Make a guess from 1 to 100");
+			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			int guess = Integer.parseInt(guessString);
 			// 5. if the guess is correct
+			if(guess == random) {
 				// 6. Win
+				JOptionPane.showMessageDialog(null, "Your win!");
+				System.exit(0);
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+			}else if(guess > random) {
 			// 7. if the guess is high
 				// 8. Tell them it's too high
+				JOptionPane.showMessageDialog(null, "Your guess is too high!");
+			}else {
 			// 9. if the guess is low
 				// 10. Tell them it's too low
-
+				JOptionPane.showMessageDialog(null, "Your guess is too low!");
+			}
+		}
 		// 13. Tell them they lose
+		JOptionPane.showMessageDialog(null, "You lose");
 		
 	}
 
